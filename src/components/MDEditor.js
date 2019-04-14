b337fea4e3db6cbb53177a0b7c9534a094ea2bd0
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-14 23:26:31 +0200

    Import React Router DOM

--- src/components/MDEditor.js
+++ src/components/MDEditor.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from "./Header"
 import Footer from "./Footer"
 import ReactMetaTags from "./MetaTags"
