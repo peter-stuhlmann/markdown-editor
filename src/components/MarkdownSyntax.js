b4773a2f090012e38e3d945cb61ffa82d377cebf
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-15 14:32:54 +0200

    Create Syntax component

---
+++ src/components/MarkdownSyntax.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class MarkdownSyntax extends Component {
+      
+    render() {
+      return (
+        <article>
+          <h1>Markdown-Syntax</h1> 
+
+        </article>
+      );
+    }
+
+}
